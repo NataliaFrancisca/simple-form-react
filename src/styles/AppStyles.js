ba76fb90320ead26189d7d8fb3557c817aeae198
Natalia Francisca <nathifrancisca@gmail.com>
2022-03-21 10:08:51 -0300

    default styles

---
+++ src/styles/AppStyles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const AppStyles = styled.main`
+    min-width: 100vw;
+    min-height: 100vh;
+    background-color: rgb(247, 240, 240);
+
+    display: flex;
+    justify-content: center;
+`
