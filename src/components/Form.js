d0ec1f5715fd6d88e0d2c5b854e1a4520edee487
Natalia Francisca <nathifrancisca@gmail.com>
2022-03-22 09:41:59 -0300

    adding fieldset to the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,31 +11,30 @@ const Form  = ({className}) => {
 
     return(
         <form onSubmit={handleSubmit(onSubmit)} className={className}>
+            <fieldset>
+                <legend>Create Account</legend>
+                <GroupInput>
+                    <label htmlFor="input-name">Name:</label>
+                    <input type="text" id="input-name" {...register('nameUser', {required: true, maxLength: 20, minLength: 2})}/>
+                    {errors.nameUser && <span className="errorMessage">This field name is required</span>}
+                </GroupInput>
 
-            <legend>Create Account</legend>
+                <GroupInput>
+                    <label htmlFor="input-email">E-mail:</label>
+                    <input type="email" id="input-email" {...register("emailUser", {required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})}/>
+                    {errors.emailUser && <span className="errorMessage">This field e-mail is required</span>}
+                </GroupInput>
 
-            <GroupInput>
-                <label htmlFor="input-name">Name:</label>
-                <input type="text" id="input-name" {...register('nameUser', {required: true, maxLength: 20, minLength: 2})}/>
-                {errors.nameUser && <span>This field name is required</span>}
-            </GroupInput>
+                <GroupInput>
+                    <label htmlFor="input-password">Password:</label>
+                    <input type="password" id="input-password"  {...register("passwordUser", {required: true})}/>
+                    {errors.passwordUser && <span className="errorMessage">This field password is required</span>}
+                </GroupInput>
 
-            <GroupInput>
-                <label htmlFor="input-email">E-mail:</label>
-                <input type="email" id="input-email" {...register("emailUser", {required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i})}/>
-                {errors.emailUser && <span>This field e-mail is required</span>}
-            </GroupInput>
-
-            <GroupInput>
-                <label htmlFor="input-password">Password:</label>
-                <input type="password" id="input-password"  {...register("passwordUser", {required: true})}/>
-                {errors.passwordUser && <span>This field password is required</span>}
-            </GroupInput>
-
-            <GroupInput>
-                <button>Subscribe</button>
-            </GroupInput>
-            
+                <GroupInput>
+                    <button>Subscribe</button>
+                </GroupInput>
+            </fieldset>
         </form>
     )
 }
